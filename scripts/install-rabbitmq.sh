54f0dc5cadfc79c4f6c4b24d76d01c1ab02f10d0
Matej <matej@merchbee.com>
2017-09-27 13:09:40 +0200

    rabbitmq_delayed_message_exchange added

--- scripts/install-rabbitmq.sh
+++ scripts/install-rabbitmq.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
 export DEBIAN_FRONTEND=noninteractive
 
-if [ -f /home/vagrant/.rabbitmq ]
+if [ ! -f /home/vagrant/.rabbitmq ]
 then
-    echo "RabbitMQ already installed."
+    touch /home/vagrant/.rabbitmq
+
+    # Update
+    apt-get install -f
+    dpkg --configure -a
+
+    # Install RabbitMQ
+    apt install -y -f rabbitmq-server
+fi
+
+if [ ! -f /home/vagrant/.rabbitmq_delayed_message_exchange ]
+then
+    wget "https://dl.bintray.com/rabbitmq/community-plugins/rabbitmq_delayed_message_exchange-0.0.1.ez"
+
+    # TODO: change destination folder based on installed rabbitmq version
+    sudo mv rabbitmq_delayed_message_exchange-0.0.1.ez /usr/lib/rabbitmq/lib/rabbitmq_server-3.5.7/plugins/
+
+    sudo rabbitmq-plugins enable rabbitmq_delayed_message_exchange
+
+    touch /home/vagrant/.rabbitmq_delayed_message_exchange
+
     exit 0
 fi
 
-touch /home/vagrant/.rabbitmq
-
-# Update
-apt-get install -f
-dpkg --configure -a
-
-# Install RabbitMQ
-apt install -y -f rabbitmq-server
-
+echo "RabbitMQ already installed."
 exit 0
+
